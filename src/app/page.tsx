515528f763b108103f176933662ad4b3eccb99ed
Dev Agrawal <agrawadv@mail.uc.edu>
2023-10-31 18:02:43 -0400

    fullstack todo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,59 @@
-import Link from "next/link";
-import { TodosComponent, Task } from "./todos";
+import { revalidatePath } from "next/cache";
+import { AddTodoComponent, TodoComponent, type Todo } from "./todos";
+import { db } from "~/server/db";
 
-export default function HomePage() {
-  const tasks: Task[] = [
-    {
-      id: "1",
-      title: "Initialize Project",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "Create Todo App",
-      completed: false,
-    },
-  ];
+export default async function HomePage() {
+  const todos = await db.todo.findMany({
+    orderBy: { createdAt: "desc" },
+  });
 
   return (
     <main>
       <h1 className="text-center text-6xl italic text-[#ef4444]">todos</h1>
       <div className="card-shadow m-auto w-1/2 min-w-[400px] rounded-lg border border-solid border-gray-300 bg-white ">
-        <TodosComponent tasks={tasks} />
+        <AddTodoComponent
+          addTodo={async (title: string) => {
+            "use server";
+
+            await db.todo.create({
+              data: {
+                title,
+                completed: false,
+              },
+            });
+
+            revalidatePath("/");
+          }}
+        />
+        {todos?.length ? (
+          todos.map((todo) => (
+            <TodoComponent
+              key={todo.id}
+              todo={todo}
+              onToggle={async () => {
+                "use server";
+
+                await db.todo.update({
+                  where: { id: todo.id },
+                  data: { completed: !todo.completed },
+                });
+
+                revalidatePath("/");
+              }}
+              onDelete={async () => {
+                "use server";
+
+                await db.todo.delete({ where: { id: todo.id } });
+
+                revalidatePath("/");
+              }}
+            />
+          ))
+        ) : (
+          <div className="flex">
+            <div className="px-3 py-2 text-gray-500">No tasks found</div>
+          </div>
+        )}
       </div>
     </main>
   );
--- src/app/todos.tsx
+++ src/app/todos.tsx
@@ -2,22 +2,32 @@
 
 import clsx from "clsx";
 
-export type Task = {
+export type Todo = {
   completed: boolean;
   title: string;
-  id: string;
+  id: number;
 };
 
-export function TodosComponent({ tasks }: { tasks: Task[] }) {
-  async function addTask() {}
-
+export function AddTodoComponent({
+  addTodo,
+}: {
+  addTodo: (title: string) => Promise<void>;
+}) {
   return (
     <>
       <form
-        onSubmit={addTask}
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const form = e.currentTarget;
+          const formData = new FormData(form);
+          const title = formData.get("title") as string;
+          await addTodo(title);
+          form.reset();
+        }}
         className="todo flex items-center border border-b border-solid px-2 py-1"
       >
         <input
+          name="title"
           placeholder="What needs to be done?"
           className="w-full p-1 outline-none placeholder:italic"
         />
@@ -25,36 +35,32 @@ export function TodosComponent({ tasks }: { tasks: Task[] }) {
           Add
         </button>
       </form>
-
-      {tasks?.length ? (
-        tasks.map((task) => <TaskComponent task={task} key={task.id} />)
-      ) : (
-        <div className="flex">
-          <div className="px-3 py-2 text-gray-500">No tasks found</div>
-        </div>
-      )}
     </>
   );
 }
 
-function TaskComponent({ task }: { task: Task }) {
-  async function toggleCompleted(task: Task) {}
-
-  async function deleteTask(task: Task) {}
-
+export function TodoComponent({
+  onDelete,
+  onToggle,
+  todo,
+}: {
+  onDelete: () => Promise<void>;
+  onToggle: () => Promise<void>;
+  todo: Todo;
+}) {
   return (
     <div className="todo flex items-center gap-4 border-b px-2 py-1">
       <input
         type="checkbox"
-        checked={task.completed}
-        onChange={() => toggleCompleted(task)}
+        defaultChecked={todo.completed}
+        onChange={() => onToggle()}
         className="h-5 w-5"
       />
-      <span className={clsx("grow p-1", task.completed && "line-through")}>
-        {task.title}
+      <span className={clsx("grow p-1", todo.completed && "line-through")}>
+        {todo.title}
       </span>
       <button
-        onClick={() => deleteTask(task)}
+        onClick={() => onDelete()}
         className="text-md rounded-full border border-none bg-white px-4 py-2 font-sans font-medium text-white hover:bg-gray-200 hover:text-inherit"
       >
         x
